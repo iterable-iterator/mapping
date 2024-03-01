7113f87f787ef760e2ba2051c2fcc11c06958c09
a flying potato <80830782+a-flying-potato@users.noreply.github.com>
2024-03-01 19:43:55 +0100

    :robot: config(xo): Configure import/order.
    These changes were automatically generated by a transform whose code can be found at:
      - https://github.com/make-github-pseudonymous-again/rejuvenate/blob/31b7e48f29789fa2e2ad9e16013ff277c3bbca57/src/transforms/xo:config-import-order.js
    Please contact the author of the transform if you believe there was an error.

--- test/src/fromMap.js
+++ test/src/fromMap.js
@@ -1,7 +1,9 @@
 import test from 'ava';
 
 import {enumerate} from '@iterable-iterator/zip';
+
 import {repr} from './_fixtures.js';
+
 import {fromMap} from '#module';
 
 const macro = (t, mapping) => {
